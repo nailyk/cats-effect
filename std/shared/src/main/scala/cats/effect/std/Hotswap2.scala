e8f30033ff411dc30f39979c72cf0c680b32486e
Morgen Peschke <morgen.peschke@gmail.com>
2025-02-06 11:24:11 -0800

    Add Hotswap2
    Squash
    - Deprecate Hotswap
    - Apply suggestions from code review
    
      Co-authored-by: Arman Bilge <armanbilge@gmail.com>
    - Remove unsafe returned value from swap
    - prePR
    - Implement Hotswap in terms of NonEmptyHotswap
    - Also adds two methods to make `NonEmptyHotswap` a drop-in replacement for `Hotswap`
    - Revert "Implement Hotswap in terms of NonEmptyHotswap"
    - This reverts commit 8dc9fcf7371fa55a495e233c64df2a70944b2179.
    - Add a not about the differences between Hotspot and NonEmptyHotspot
    - Revert "Add a not about the differences between Hotspot and NonEmptyHotspot"
    - This reverts commit bcfb4b0a49dbc838317d1025a2c9aa2bfe8716b7.
    - Revert "Revert "Implement Hotswap in terms of NonEmptyHotswap""
    - This reverts commit 4b7ebe499260bdd41ca86516f8adc8333d20a391.
    - Implement lock aquiring shenanigans and restore related test
    
      Thanks to armanbilge for the [suggestion](https://github.com/typelevel/cats-effect/pull/4267#discussion_r1947290748)
    - Switch NonEmptyHotswap to Hotswap2
    - Apply suggestions from code review
    
      Co-authored-by: Arman Bilge <armanbilge@gmail.com>
    - Deprecate Hotswap
    - Documentation fixes in Hotswap2
    - Add Hotswap2.getOpt
    
      This would allow upgrading from Hotswap without losing the semantics of not acquiring a lock when no resource is present for `Hotswap2[F, Resource[Option[R]]]`

---
+++ std/shared/src/main/scala/cats/effect/std/Hotswap2.scala
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2020-2025 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.std
+
+import cats.effect.kernel.Resource.ExitCase.Succeeded
+import cats.effect.kernel.{Concurrent, MonadCancel, Ref, Resource}
+import cats.syntax.all.*
+
+/**
+ * A concurrent data structure that exposes a linear sequence of `R` resources as a single
+ * [[cats.effect.kernel.Resource]] in `F` without accumulation.
+ *
+ * A [[Hotswap2]] is allocated within a [[cats.effect.kernel.Resource]] that dictates the scope
+ * of its lifetime. After creation, a `Resource[F, R]` can be swapped in by calling [[swap]].
+ * The newly acquired resource is returned and is released either when the [[Hotswap2]] is
+ * finalized or upon the next call to [[swap]], whichever occurs first.
+ *
+ * The following diagram illustrates the linear allocation and release of three resources `r1`,
+ * `r2`, and `r3` cycled through [[Hotswap2]]:
+ *
+ * {{{
+ * create(r1) ----- swap(r2) ---- swap(r3) ---- X
+ * |                |             |             |
+ * r1 acquired      |             |             |
+ *                  r2 acquired   |             |
+ *                  r1 released   r3 acquired   |
+ *                                r2 released   |
+ *                                              r3 released
+ * }}}
+ *
+ * [[Hotswap2]] is particularly useful when working with effects that cycle through resources,
+ * like writing bytes to files or rotating files every N bytes or M seconds. Without
+ * [[Hotswap2]], such effects leak resources: on each file rotation, a file handle or some
+ * internal resource handle accumulates. With [[Hotswap2]], the only registered resource is the
+ * [[Hotswap2]] itself, and each file is swapped in only after swapping the previous one out.
+ *
+ * Replaces the deprecated [[Hotswap]] with a safer API.
+ */
+sealed trait Hotswap2[F[_], R] {
+
+  /**
+   * Allocates a new resource and closes the previous one.
+   *
+   * When the lifetime of the [[Hotswap2]] is completed, the resource allocated by the most
+   * recent [[swap]] will be finalized.
+   *
+   * [[swap]] finalizes the previous resource immediately, so users must ensure that the old `R`
+   * is not used thereafter. Failure to do so may result in an error on the _consumer_ side. In
+   * any case, no resources will be leaked.
+   *
+   * To access the current resource, use [[get]], which guarantees that it will not be released
+   * while it is being used.
+   *
+   * If [[swap]] is called after the lifetime of the [[Hotswap2]] is over, it will raise an
+   * error, but will ensure that all resources are finalized before returning.
+   */
+  def swap(next: Resource[F, R]): F[Unit]
+
+  /**
+   * Gets the current resource. The returned resource is guaranteed to be available for the
+   * duration of the returned resource.
+   */
+  def get: Resource[F, R]
+}
+
+object Hotswap2 {
+
+  /**
+   * Creates a new [[Hotswap2]] initialized with the specified resource, which represents a
+   * [[cats.effect.kernel.Resource]] that can be swapped during the lifetime of this
+   * [[Hotswap2]].
+   */
+  def apply[F[_], R](initial: Resource[F, R])(
+      implicit F: Concurrent[F]): Resource[F, Hotswap2[F, R]] =
+    Resource.eval(Semaphore[F](Long.MaxValue)).flatMap { semaphore =>
+      sealed abstract class State
+      case class Acquired(r: R, fin: F[Unit]) extends State
+      case object Finalized extends State
+
+      def initialize: F[Ref[F, State]] =
+        F.uncancelable { poll =>
+          poll(initial.allocated).flatMap {
+            case (r, fin) =>
+              exclusive.mapK(poll).onCancel(Resource.eval(fin)).surround {
+                F.ref(Acquired(r, fin))
+              }
+          }
+        }
+
+      def finalize(state: Ref[F, State]): F[Unit] =
+        state.getAndSet(Finalized).flatMap {
+          case Acquired(_, finalizer) => exclusive.surround(finalizer)
+          case Finalized => raise("NonEmptyHotswap already finalized")
+        }
+
+      def raise[A](message: String): F[A] =
+        F.raiseError[A](new IllegalStateException(message))
+
+      def exclusive: Resource[F, Unit] =
+        Resource.makeFull[F, Unit](poll => poll(semaphore.acquireN(Long.MaxValue)))(_ =>
+          semaphore.releaseN(Long.MaxValue))
+
+      Resource.make(initialize)(finalize).map { state =>
+        new Hotswap2[F, R] {
+
+          override def swap(next: Resource[F, R]): F[Unit] =
+            F.uncancelable { poll =>
+              poll(next.allocated).flatMap {
+                case (r, fin) =>
+                  exclusive.mapK(poll).onCancel(Resource.eval(fin)).surround {
+                    swapFinalizer(Acquired(r, fin))
+                  }
+              }
+            }
+
+          override def get: Resource[F, R] =
+            Resource.makeFull[F, R] { poll =>
+              poll(semaphore.acquire) *> // acquire shared lock
+                state.get.flatMap {
+                  case Acquired(r, _) => F.pure(r)
+                  case _ => raise("Hotswap already finalized")
+                }
+            }(_ => semaphore.release)
+
+          private def swapFinalizer(next: State): F[Unit] =
+            state.flatModify {
+              case Acquired(_, fin) =>
+                next -> fin
+              case Finalized =>
+                val fin = next match {
+                  case Acquired(_, fin) => fin
+                  case _ => F.unit
+                }
+                Finalized -> (fin *> raise[Unit]("Cannot swap after finalization"))
+            }
+        }
+      }
+    }
+
+  /**
+   * Creates a [[Hotswap2]] of `Resource[F, Option[R]]` containing a `None`
+   */
+  def empty[F[_], R](implicit F: Concurrent[F]): Resource[F, Hotswap2[F, Option[R]]] =
+    apply[F, Option[R]](Resource.pure(none))
+
+  implicit final class Hotswap2OptionalResourcesOpt[F[_], R](
+      private val hs: Hotswap2[F, Option[R]])
+      extends AnyVal {
+
+    /**
+     * When the [[Resource]] contained by a [[Hotswap2]] is wrapped in an [[Option]] it is not
+     * desirable to prevent calls to [[Hotswap2.swap]] when the [[Resource]] contains [[None]].
+     *
+     * [[getOpt]] preserves this behavior from [[Hotswap.get]]
+     */
+    def getOpt(implicit F: MonadCancel[F, Throwable]): Resource[F, Option[R]] =
+      Resource.applyFull[F, Option[R]] { poll =>
+        poll(hs.get.allocatedCase).flatMap {
+          case (None, fin) => fin(Succeeded) *> F.pure((None, _ => F.unit))
+          case (r, fin) => F.pure((r, fin))
+        }
+      }
+
+    def clear: F[Unit] = hs.swap(Resource.pure(none))
+  }
+}
