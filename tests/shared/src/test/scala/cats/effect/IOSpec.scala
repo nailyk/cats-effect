53f5fe4465a5f7dfe1b3344e8423c3421ba3ef56
Shrey Pant <pantshrey01@gmail.com>
2025-04-07 03:36:59 +0530

    Added test for handling large Ref update chains with syncStep

--- tests/shared/src/test/scala/cats/effect/IOSpec.scala
+++ tests/shared/src/test/scala/cats/effect/IOSpec.scala
@@ -2130,6 +2130,22 @@ class IOSpec extends BaseSpec with Discipline with IOPlatformSpecification {
           .syncStep(Int.MaxValue)
         sio.map(_.bimap(_ => (), _ => ())) must completeAsSync(Right(()))
       }
+
+      "handle large Ref update chain without StackOverflowError (#4337)" in {
+        val syncStepSize = 1000000
+        val updates = 100000
+
+        val io = for {
+          ref <- Ref.of[IO, Int](0)
+          _ <- Vector.fill(updates)(1).traverse_[IO, Unit](n => ref.update(_ + n))
+          result <- ref.get
+        } yield result
+
+        io.syncStep(syncStepSize).map {
+          case Left(_) => throw new RuntimeException("Expected synchronous completion")
+          case Right(n) => n
+        } must completeAsSync(updates)
+      }
     }
 
     "fiber repeated yielding test" in real {
